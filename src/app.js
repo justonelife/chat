8ff3c0e57abaf4d0fe7492c5201db235671b1931
justonelife <ngnhut07051998@outlook.com>
2020-04-23 18:01:10 +0700

    init reactjs

---
+++ src/app.js
@@ -0,0 +1,15 @@
+class AppChat extends React.Component {
+
+	render() {
+		return (
+			<React.Fragment>
+				<h1>Hello</h1>
+			</React.Fragment>
+		);
+	}
+}
+
+ReactDOM.render(
+	<AppChat />,
+	document.getElementById('app')
+);
